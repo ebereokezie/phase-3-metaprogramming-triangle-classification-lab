3d0543f460dd9810e9471d337fd00590c608da7e
Ebere Okezie <ebereokezie@gmail.com>
2022-08-18 15:51:52 -0500

    commit tree 341dfa3ed9ab26960f43ee483b3694c1f9492bbb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+  
+  def kind
+    if (@length1 == @length2 && @length2 == @length3) && (@length1 > 0 || @length2 > 0 || @length3 > 0) && ((@length1 + @length2 )>@length3 && (@length2 + @length3 )>@length1 && (@length1 + @length3 )>@length2)     
+      :equilateral
+    elsif (@length1 == @length2 || @length1 == @length3 || @length2 == @length3) && (@length1 > 0 || @length2 > 0 || @length3 > 0) && ((@length1 + @length2 )>@length3 && (@length2 + @length3 )>@length1 && (@length1 + @length3 )>@length2) 
+      :isosceles
+    elsif ((@length1 != @length2) && (@length2 != @length3) && (@length1 != @length3)) && (@length1 > 0 || @length2 > 0 || @length3 > 0) && ((@length1 + @length2 )>@length3 && (@length2 + @length3 )>@length1 && (@length1 + @length3 )>@length2) 
+      :scalene
+    else 
+      raise TriangleError
+    end
+
+
+  end
+
+  class TriangleError < StandardError
+  end
 end
